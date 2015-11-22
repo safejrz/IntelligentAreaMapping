457968d51c05c72da25894521c0420fd9c16536b
Javier Rivera <ingjrz@gmail.com>
2015-11-22 03:23:41 -0600

    Frame replacement technique implemented successfully! XD

--- app/src/main/java/com/iam/intelligentareamapping/MainActivity.java
+++ app/src/main/java/com/iam/intelligentareamapping/MainActivity.java
@@ -1,6 +1,7 @@
 package com.iam.intelligentareamapping;
 
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
@@ -44,8 +45,8 @@ public class MainActivity extends AppCompatActivity
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        //FragmentManager fragmentManager = getFragmentManager();
-        //fragmentManager.beginTransaction().replace(R.id.content_frame, FragmentBlank.getInstance()).commit();
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_frame, FragmentBlank.getInstance()).commit();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
